7b93e731d08d3598466080a91b6f33cec7c731e2
takakikasuga <takaki.soccer1@gmail.com>
2023-03-09 09:47:29 +0900

    React18のTransitionの確認

--- react18-implementation-react17/src/App.tsx
+++ react18-implementation-react17/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
+import { AutoBatchOther } from './components/AutoBatchOther';
 
 function App() {
   console.log('App is Rendering for react17');
@@ -9,19 +10,8 @@ function App() {
   }, []);
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <AutoBatchEventHandler />
+      <AutoBatchOther />
     </div>
   );
 }
--- react18-implementation-react18/src/App.tsx
+++ react18-implementation-react18/src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
+import { AutoBatchOther } from './components/AutoBatchOther';
+import { Transition } from './components/Transition';
 
 function App() {
   console.log('App is Rendering for react18');
@@ -8,21 +10,10 @@ function App() {
     console.log('App is Rendering for react18 in useEffect');
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <AutoBatchEventHandler />
+      <AutoBatchOther />
+      <Transition />
     </div>
   );
 }
